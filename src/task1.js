1839ac8d98b96953849994c6b1b06ba952106d18
Uroojfatimashk2004 <uroojfatimashaikh04@gmail.com>
2024-07-03 14:19:27 +0500

    update

---
+++ src/task1.js
@@ -0,0 +1,28 @@
+import  styles from'./App.module.css';
+
+function App (){
+return (
+
+<div className={styles.App}>
+<h1  className={styles.name}>LAJ</h1>
+
+
+</div>
+
+
+    
+);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+export default App;
